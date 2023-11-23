3b85f7b2ed2d5cdbbf5bdb3d55b368d0f4daa385
m-sobala <md.sobala@gmail.com>
2023-11-23 17:02:01 +0100

    changes

--- src/regression_plotter.py
+++ src/regression_plotter.py
@@ -1,9 +1,8 @@
-from multiple_linear_regression import MultipleLinearRegression
 import matplotlib.pyplot as plt
 
 class ReggresionPlotter:
-    def __init__(self):
-        self.status = 1
+    def __init__(self, model):
+        self.model = model
     
     def plot_singular_feature(self, X, Y):
         assert X.shape[1] == 1 or X.shape[1] == 0, 'Wrong input dimensions'
@@ -11,11 +10,18 @@ class ReggresionPlotter:
         X = X.reshape((-1, 1))
         Y = Y.reshape((-1, 1))
 
-        model = MultipleLinearRegression()
-        model.train(X, Y)
-        predictions = model.predict(X)
+        predictions = self.model.predict(X)
 
         plt.plot(X, predictions ,color='k')
         plt.scatter(X, Y, color='r')
         plt.show()
-        
+    
+    def plot_two_features(self, X, Y):
+        assert X.shape[1] == 2, 'Wrong input dimensions'
+
+        Y = Y.reshape((-1, 1))
+
+        predictions = self.model.predict(X)
+
+        plt.plot(X, predictions ,color='k', projection='3d')
+        plt.show()
